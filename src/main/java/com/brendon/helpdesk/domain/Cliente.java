1eb0d914200dc4d9fd98dfc30994a0399831cc55
brendongama <brendon_gama@hotmail.com>
2021-10-19 20:26:19 -0300

    Protegendo aplicação contra serealização

--- src/main/java/com/brendon/helpdesk/domain/Cliente.java
+++ src/main/java/com/brendon/helpdesk/domain/Cliente.java
@@ -8,11 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.brendon.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente extends Pessoa implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
