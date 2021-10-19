0b95559d40484d1aea8cc0c3496671dabe44858f
brendongama <brendon_gama@hotmail.com>
2021-10-19 17:20:18 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/brendon/helpdesk/domain/Cliente.java
+++ src/main/java/com/brendon/helpdesk/domain/Cliente.java
@@ -1,14 +1,24 @@
 package com.brendon.helpdesk.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+import com.brendon.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
