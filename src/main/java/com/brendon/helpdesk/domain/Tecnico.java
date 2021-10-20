f9d686aa1f1a9d4c123f558dfcea5d35b07456a1
brendongama <brendon_gama@hotmail.com>
2021-10-20 13:07:41 -0300

    EndPoint Create

--- src/main/java/com/brendon/helpdesk/domain/Tecnico.java
+++ src/main/java/com/brendon/helpdesk/domain/Tecnico.java
@@ -3,10 +3,12 @@ package com.brendon.helpdesk.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.brendon.helpdesk.domain.dtos.TecnicoDTO;
 import com.brendon.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -25,6 +27,18 @@ public class Tecnico extends Pessoa implements Serializable{
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
